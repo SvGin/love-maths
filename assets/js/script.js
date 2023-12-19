6bba5e2e8de6edf5a9620ea55104e285be7a4e01
SvGin <>
2023-12-19 13:09:22 +0000

    Add multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,7 +32,9 @@ function runGame(gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
-    } else {
+    } else if (gameType === "multiply"){
+        displayMultiplyQueation(num1,num2);
+    }else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
@@ -72,6 +74,8 @@ function calculateCorrectAnswer(){
 
     if (operator === '+') {
         return [operand1 + operand2, "addition"];
+    } else if (operator === 'x') {
+        return [operand1 * operand2, "multiply"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting`;
@@ -110,6 +114,10 @@ function displaySubtractQuestion(){
 
 }
 
-function displayMultiplyQueation(){
+function displayMultiplyQueation(operand1, operand2){
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 
 }
